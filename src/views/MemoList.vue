e9072377306aeeb388bd60dfac4cdfc0692b211b
kazu0914 <kazu0914.dev@gmail.com>
2020-09-13 19:07:29 +0900

    add memolist page

--- src/views/MemoList.vue
+++ src/views/MemoList.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-      <p>一覧画面</p>
+  <div class="list">
+    <ul v-for="memo in newest" v-bind:key="memo.id">
+      <li>{{ memo.body }}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: "list",
+  computed: {
+    newest: function() {
+      return this.$store.state.memos.slice().reverse();
+    },
+  },
+};
+</script>
